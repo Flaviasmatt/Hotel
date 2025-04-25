caf3a96af8faabdbb3e24eee86e19cde0b5917e0
Flaviasmatt <flaviasmattisson@gmail.com>
2025-04-25 09:52:30 +0200

    remove .env and redis.js from history

--- redis.js
+++
@@ -1,7 +0,0 @@
-const { createClient } = require('redis');
-const redisClient = createClient({
-  url: "rediss://default:AVNS_R77IMVkRekEiEhkQjUj@redis-34b9f5ee-teacher-101.d.aivencloud.com:21247"
-});
-redisClient.connect().catch(console.error);
-
-module.exports = redisClient;
